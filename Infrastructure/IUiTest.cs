58445ed46401962ad45132c136407d66886fa8cf
Majdi Barrat <19928575+DotNetWebAppDeveloper@users.noreply.github.com>
2019-05-07 14:49:53 +0200

    Add files via upload

---
+++ Infrastructure/IUiTest.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace SeleniumTestExample.Infrastructure
+{
+    public interface IUiTest
+    {
+        //void Execute();
+        void Execute(DataRow parameter);
+    }
+}
